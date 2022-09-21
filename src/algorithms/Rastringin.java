f233134e640ebd67e33b275b4e0ac8481d1796fa
manuel <mjmp0027>
2022-09-21 15:34:15 +0200

    Función Rastringin terminada(se sugiere ojeada)

--- src/algorithms/Rastringin.java
+++ src/algorithms/Rastringin.java
@@ -1,4 +1,12 @@
 package algorithms;
 
 public class Rastringin {
+    static public double evaluateR(double[] genotype) {
+        double sum = 0.0;
+        double d = 2;
+        for (int i = 0; i < d; i++){
+            sum += Math.pow(genotype[i],2) - 10 * Math.cos(2*Math.PI*genotype[i]);
+        }
+        return -10 * d + sum;
+    }
 }
