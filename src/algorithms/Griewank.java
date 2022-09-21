e294bd5dd7ec73fbb2aba67d3a8cb326022c1cdc
manuel <mjmp0027>
2022-09-21 14:51:26 +0200

    Función Griewank terminada(se sugiere ojeada)

--- src/algorithms/Griewank.java
+++ src/algorithms/Griewank.java
@@ -1,4 +1,17 @@
 package algorithms;
 
 public class Griewank {
+    static public double fitnessFunc( double[] chromosome) {
+        double part1 = 0;
+        double part2 = 1;
+        for (int i = 0; i < chromosome.length; i++) {
+            part1 += chromosome[i] * 2 * 2;
+
+            for (int j = 0; j < chromosome.length; j++) {
+                part2 *= Math.cos((chromosome[i]) / Math.sqrt(i + 1));
+
+            }
+        }
+        return (part1 / 4000.0) - part2 + 1;
+    }
 }
