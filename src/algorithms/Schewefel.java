52ac9f910b2b2faf81c5c96e5996815996a41ef0
manuel <mjmp0027>
2022-09-22 11:38:39 +0200

    Función Schewefel terminada(se sugiere ojeada)

--- src/algorithms/Schewefel.java
+++ src/algorithms/Schewefel.java
@@ -1,4 +1,13 @@
 package algorithms;
 
 public class Schewefel {
+    static public double evaluateS(double[] genotype) {
+        double sum = 0.0;
+        double d = 2;
+        for (int i = 0; i < d; i++) {
+            sum += (genotype[i] * Math.sin(Math.sqrt(Math.abs(genotype[i]))));
+        }
+        return -418.9829 * d - sum;
+
+    }
 }
