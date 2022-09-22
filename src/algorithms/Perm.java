e6223d1f85ece0317c8ddf62a5a5bd04004d6990
manuel <mjmp0027>
2022-09-22 12:04:22 +0200

    Función RotatedHypeEllipsoid terminada(se sugiere ojeada) y función Perm sin terminar no se el fallo...

--- src/algorithms/Perm.java
+++ src/algorithms/Perm.java
@@ -1,4 +1,14 @@
 package algorithms;
 
 public class Perm {
+    static public double evaluateP(double[] genotype) {
+        double sum = 0.0;
+        double d = 2;
+        for (int i = 1; i < d; i++) {
+            for (int j = 1; j < d; j++) {
+                sum += Math.pow(2, ((j + 10) * (genotype[j] ^ i - (1 / j ^ i))));
+            }
+        }
+        return sum;
+    }
 }
--- src/algorithms/RotatedHypeEllipsoid.java
+++ src/algorithms/RotatedHypeEllipsoid.java
@@ -1,4 +1,14 @@
 package algorithms;
 //Pedro
 public class RotatedHypeEllipsoid {
+    static public double evaluateR(double[] genotype){
+        double sum = 0.0;
+        double d = 2;
+        for (int i = 0; i < d; i++) {
+            for (int j = 0; j < i; j++) {
+                sum += Math.pow(2, genotype[j]);
+            }
+        }
+        return sum;
+    }
 }
