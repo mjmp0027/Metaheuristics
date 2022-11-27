fb5e1bf899af2cc875aa9548634dfb9b01df410d
pcc00031 <pcc00031@red.ujaen.es>
2022-11-27 12:43:25 +0100

    Lectura nuevo archivo

---
+++ src/main/java/uja/meta/utils/Daido.java
@@ -0,0 +1,13 @@
+package uja.meta.utils;
+
+import lombok.Data;
+
+@Data
+public class Daido {
+    private double dni;
+    private double ape;
+    private double temp_amb;
+    private double vel_viento;
+    private double smr;
+    private double potencia;
+}
